24233a8c4f8305b5146b3f4a2fba54119bfcf3d3
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-07 18:56:38 +0500

    3/24

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const asciiArt1 = document.getElementById("ascii-art");
     const asciiArt2 = document.getElementById("ascii-art-2");
     const reverseButton = document.getElementById('reverse');
-    const entertainButton = document.getElementById('entertain-yourself');
 
-    // Function to toggle between images when clicked on the actual ASCII content
     function toggleImage(event) {
-        // Check if the click was on a <pre> element containing the image
         if (event.target.tagName === "PRE") {
             if (asciiArt1.style.display === "none") {
                 asciiArt1.style.display = "block";
@@ -18,17 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Add event listeners to both images
     asciiArt1.addEventListener("click", toggleImage);
     asciiArt2.addEventListener("click", toggleImage);
 
-    // Reverse the colors
     reverseButton.addEventListener('click', function() {
-        console.log('Button clicked!');
         document.body.classList.toggle('reverse-colors');
-        
-        // Also toggle the button color scheme
         reverseButton.classList.toggle('reverse-colors');
-        entertainButton.classList.toggle('reverse-colors');
     });
 });
