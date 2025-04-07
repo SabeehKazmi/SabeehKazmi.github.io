4141223d9fae35f4b719b502db07c596f6e09570
SabeehKazmi <syed.hassan.566148@gmail.com>
2025-04-07 19:17:29 +0500

    4/24

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const asciiArt1 = document.getElementById("ascii-art");
     const asciiArt2 = document.getElementById("ascii-art-2");
-    const reverseButton = document.getElementById('reverse');
+    const reverseButton = document.getElementById("reverse");
 
-    function toggleImage(event) {
-        if (event.target.tagName === "PRE") {
-            if (asciiArt1.style.display === "none") {
-                asciiArt1.style.display = "block";
-                asciiArt2.style.display = "none";
-            } else {
-                asciiArt1.style.display = "none";
-                asciiArt2.style.display = "block";
-            }
-        }
+    function toggleImage() {
+        const showingFirst = asciiArt1.style.display !== "none";
+
+        asciiArt1.style.display = showingFirst ? "none" : "block";
+        asciiArt2.style.display = showingFirst ? "block" : "none";
     }
 
     asciiArt1.addEventListener("click", toggleImage);
     asciiArt2.addEventListener("click", toggleImage);
 
-    reverseButton.addEventListener('click', function() {
-        document.body.classList.toggle('reverse-colors');
-        reverseButton.classList.toggle('reverse-colors');
+    reverseButton.addEventListener("click", function () {
+        document.body.classList.toggle("reverse-colors");
+        reverseButton.classList.toggle("reverse-colors");
     });
 });
